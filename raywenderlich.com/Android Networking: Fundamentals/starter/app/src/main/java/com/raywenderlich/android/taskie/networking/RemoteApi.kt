c4346116a17ed6a6bfd00bf892f6d7b12f477905
amzesmoro <amzesmoro05@gmail.com>
2020-07-09 11:27:24 +0700

    Create An HTTP Connection on registerUser() function

--- raywenderlich.com/Android Networking: Fundamentals/starter/app/src/main/java/com/raywenderlich/android/taskie/networking/RemoteApi.kt
+++ raywenderlich.com/Android Networking: Fundamentals/starter/app/src/main/java/com/raywenderlich/android/taskie/networking/RemoteApi.kt
@@ -38,6 +38,11 @@ import com.raywenderlich.android.taskie.model.Task
 import com.raywenderlich.android.taskie.model.UserProfile
 import com.raywenderlich.android.taskie.model.request.AddTaskRequest
 import com.raywenderlich.android.taskie.model.request.UserDataRequest
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.StringBuilder
+import java.net.HttpURLConnection
+import java.net.URL
 
 /**
  * Holds decoupled logic for all the API calls.
@@ -47,51 +52,88 @@ const val BASE_URL = "https://taskie-rw.herokuapp.com"
 
 class RemoteApi {
 
-  fun loginUser(userDataRequest: UserDataRequest, onUserLoggedIn: (String?, Throwable?) -> Unit) {
-    onUserLoggedIn("token", null)
-  }
+    fun loginUser(userDataRequest: UserDataRequest, onUserLoggedIn: (String?, Throwable?) -> Unit) {
+        onUserLoggedIn("token", null)
+    }
 
-  fun registerUser(userDataRequest: UserDataRequest, onUserCreated: (String?, Throwable?) -> Unit) {
-    onUserCreated("Success!", null)
-  }
+    fun registerUser(userDataRequest: UserDataRequest, onUserCreated: (String?, Throwable?) -> Unit) {
+        Thread(Runnable {
+            val connection = URL("$BASE_URL/api/register").openConnection() as HttpURLConnection
+            connection.requestMethod = "POST"
+            connection.setRequestProperty("Content-Type", "application/json")
+            connection.setRequestProperty("Accept", "application/json")
+            connection.readTimeout = 10000
+            connection.connectTimeout = 10000
+            connection.doOutput = true
+            connection.doInput = true
 
-  fun getTasks(onTasksReceived: (List<Task>, Throwable?) -> Unit) {
-    onTasksReceived(listOf(
-        Task("id",
-            "Wash laundry",
-            "Wash the whites and colored separately!",
-            false,
-            1
-        ),
-        Task("id2",
-            "Do some work",
-            "Finish the project",
-            false,
-            3
+            val body = "{\"name\":\"${userDataRequest.name}\", \"email\":\"${userDataRequest.email}\"," +
+                    "\"password\":\"${userDataRequest.password}\"}"
+            val bytes = body.toByteArray()
+
+            try {
+                connection.outputStream.use { outputStream ->
+                    outputStream.write(bytes)
+                }
+
+                val reader = InputStreamReader(connection.inputStream)
+
+                reader.use { input ->
+                    val response = StringBuilder()
+                    val bufferedReader = BufferedReader(input)
+
+                    bufferedReader.useLines { lines ->
+                        lines.forEach {
+                            response.append(it.trim())
+                        }
+                    }
+
+                    onUserCreated(response.toString(), null)
+                }
+            } catch (error: Throwable) {
+                onUserCreated(null, error)
+            }
+            connection.disconnect()
+        }).start()
+    }
+
+    fun getTasks(onTasksReceived: (List<Task>, Throwable?) -> Unit) {
+        onTasksReceived(listOf(
+                Task("id",
+                        "Wash laundry",
+                        "Wash the whites and colored separately!",
+                        false,
+                        1
+                ),
+                Task("id2",
+                        "Do some work",
+                        "Finish the project",
+                        false,
+                        3
+                )
+        ), null)
+    }
+
+    fun deleteTask(onTaskDeleted: (Throwable?) -> Unit) {
+        onTaskDeleted(null)
+    }
+
+    fun completeTask(onTaskCompleted: (Throwable?) -> Unit) {
+        onTaskCompleted(null)
+    }
+
+    fun addTask(addTaskRequest: AddTaskRequest, onTaskCreated: (Task?, Throwable?) -> Unit) {
+        onTaskCreated(
+                Task("id3",
+                        addTaskRequest.title,
+                        addTaskRequest.content,
+                        false,
+                        addTaskRequest.taskPriority
+                ), null
         )
-    ), null)
-  }
+    }
 
-  fun deleteTask(onTaskDeleted: (Throwable?) -> Unit) {
-    onTaskDeleted(null)
-  }
-
-  fun completeTask(onTaskCompleted: (Throwable?) -> Unit) {
-    onTaskCompleted(null)
-  }
-
-  fun addTask(addTaskRequest: AddTaskRequest, onTaskCreated: (Task?, Throwable?) -> Unit) {
-    onTaskCreated(
-        Task("id3",
-            addTaskRequest.title,
-            addTaskRequest.content,
-            false,
-            addTaskRequest.taskPriority
-        ), null
-    )
-  }
-
-  fun getUserProfile(onUserProfileReceived: (UserProfile?, Throwable?) -> Unit) {
-    onUserProfileReceived(UserProfile("mail@mail.com", "Filip", 10), null)
-  }
+    fun getUserProfile(onUserProfileReceived: (UserProfile?, Throwable?) -> Unit) {
+        onUserProfileReceived(UserProfile("mail@mail.com", "Filip", 10), null)
+    }
 }
